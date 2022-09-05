fe5f5d2048207824ac4a0dbfd8839e415ab47326
JonathanTanggg <jonathantang1998@outlook.com>
2022-09-05 23:47:48 +0200

    display a single phonebook entry

--- index.js
+++ index.js
@@ -4,39 +4,50 @@ const time = require('express-timestamp')
 const app = express()
 app.use(time.init)
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-  ]
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
   app.get('/api/persons', (request, response) => {
-    response.json(notes)
+    response.json(persons)
   })
 
   app.get('/info', (request, response) => {
-    const entries = notes.length
-    
+    const entries = persons.length
+
     response.send(`<div>
     <p>Phonebook has info for ${entries} people.</p>
     <p>${request.timestamp}</p>
     </div>`)
     
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+    if (!person) {
+      response.status(404).end()
+    }
+    response.json(person)
   })
 
   const PORT = 3001
