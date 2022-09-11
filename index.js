0f054d0db0017c442fb67fc50df182ffe309d1e6
唐圣灵 <jonathan@tangshenglingdeMacBook-Air.local>
2022-09-11 11:44:48 +0200

    added some error checks

--- index.js
+++ index.js
@@ -62,9 +62,15 @@ let persons = [
 
     const body = request.body
 
-    if(!body) {
+    if(!body.name || !body.number) {
       return response.status(400).json({
         error: 'content missing'
+      })
+    }
+
+    if(persons.find(person => person.name === body.name)) {
+      return response.status(409).json({
+        error: 'name must be unique'
       })
     }
 
